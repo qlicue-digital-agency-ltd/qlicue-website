4790a55d14ff4362e97a77f956e3960198b0ba41
ProDeveloper-1 <prodeveloper-1@localhost.localdomain>
2020-05-05 06:20:00 -0400

    Changed the banner's background

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,15 +6,15 @@ window.onscroll = function() {
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById('main-header').style.backgroundColor = '#d3d3d3';
-        document.getElementById('main-header').style.boxShadow = '0px 0px 5px #d3d3d3';
+        document.getElementById('main-header').style.backgroundColor = '#004bff';
+        // document.getElementById('main-header').style.boxShadow = '0px 0px 0px #007bff';
         document.getElementById('main-header').style.height = '70px';
         document.getElementById('logo').style.width = '80px';
     }
 
     else{
-        document.getElementById('main-header').style.backgroundColor = '#f8f9fa!important';
-        document.getElementById('main-header').style.boxShadow = '0px 0px 0px';
+        document.getElementById('main-header').style.backgroundColor = '';
+        // document.getElementById('main-header').style.boxShadow = '0px 0px 0px';
         document.getElementById('main-header').style.height = '140px';
         document.getElementById('logo').style.width = '110px';
     }
