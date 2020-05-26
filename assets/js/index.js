2ad6c57f2036e31916d8aba48dbb9689c95cbb4f
ProDeveloper-1 <prodeveloper-1@localhost.localdomain>
2020-05-25 23:08:27 -0400

    Changes on banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,7 @@ function scrollFunction() {
         // document.getElementById('main-header').style.boxShadow = '0px 0px 0px #007bff';
         document.getElementById('main-header').style.height = '70px';
         document.getElementById('logo').style.width = '80px';
+        // document.getElementById('logo').style.background = '#f8f9fa';
     }
 
     else{
@@ -17,5 +18,6 @@ function scrollFunction() {
         // document.getElementById('main-header').style.boxShadow = '0px 0px 0px';
         document.getElementById('main-header').style.height = '140px';
         document.getElementById('logo').style.width = '110px';
+        // document.getElementById('logo').style.background = '#f8f9fa';
     }
 }
